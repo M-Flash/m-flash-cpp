9ce5bb29c89cd01bfa4a7dbf73e578d7a573bd98
hugo <hugo@localhost.localdomain>
2015-07-20 09:10:50 -0300

    TurboGraph formatter Updated

--- test/PreprocessorTest.cpp
+++ test/PreprocessorTest.cpp
@@ -20,26 +20,29 @@ int main(int argc, char* argv[]){
 	int64 buffer_size = 8 * 100;
 */
 
+
 /*
 
 	//YAHOO
-	std::string graph_file = "/hugo/datasets/yahoo/yahoo";
+	std::string graph_file = "/run/media/hugo/data/datasets/yahoo";
 	int64 vertices_partition = 1073741824;
 	int64 buffer_size = 1024L*1024*1024 * 8;
 	bool edgelist = false;
 	char separator = ' ';
 */
 
+
+
 /*
 
 	//Twitter
-	std::string graph_file = "/hugo/datasets/twitter/twitter";
+	std::string graph_file = "/run/media/hugo/data/datasets/twitter";
 	int64 vertices_partition = 41652231;
 	int64 buffer_size = 1024L*1024*1024 * 8;
 	bool edgelist = true;
 	char separator = ' ';
-*/
 
+*/
 
 
 	//LiveJournal
--- test/TurboGraphFormatter.cpp
+++ test/TurboGraphFormatter.cpp
@@ -2,7 +2,7 @@
 #include "../src/core/mapped_stream.hpp"
 #include "../log/easylogging++.h"
 #include "../src/core/splitterbuffer.hpp"
-
+#include <stdio.h>
 
 //INITIALIZE_EASYLOGGINGPP
 
@@ -10,30 +10,42 @@ using namespace mflash;
 int main(){
 	//std::string graph_file = "/hugo/datasets/tiny-graph/tiny-graph";
 
-
 	//std::string graph_file = "/hugo/datasets/twitter/twitter";
 
 
-	/*
-	 * YAHOO
-	 */
-	std::string graph_file = "/run/media/hugo/hugo/datasets/lj/.M-FLASH/lj/0_0.block";
+	//LJ
+	/*std::string graph_file = "/run/media/hugo/hugo/datasets/lj/.M-FLASH/lj/0_0.block";
+	int64 edges = 68993773;
+	int64 max_vertex_id = 4847573;
+	*/
+
+	//YAHOO
+/*
+	std::string graph_file = "/run/media/hugo/hugo/datasets/yahoo1/0.partition";
 	int64 edges = 6636600779;
 	int64 max_vertex_id = 1413511393;
+*/
 
-	std::ofstream file;
-	file.open(graph_file + ".txt");
+
+	//TWITTER
+
+	std::string graph_file = "/run/media/hugo/hugo/datasets/twitter/.M-FLASH/0_0.block";
+	int64 edges = 1468365182;
+	int64 max_vertex_id = 41652231;
+
+
+	FILE* fout = fopen("/run/media/hugo/data/frameworks/turbograph/Turbograph/KDDResults/data/twitter.txt", "w");
 
 	MappedStream stream (graph_file);
 
-	file<<max_vertex_id << " "<<edges<<std::endl;
+	fprintf(fout, "%lu %lu\n", max_vertex_id, edges);
 	int from, to, idx;
 	while(stream.has_remain()){
 		from = stream.next_int();
 		to = stream.next_int();
-		file<<from << " "<<to<<std::endl;
+		fprintf(fout, "%d %d\n", from, to);
 	}
-	file.close();
+	fclose(fout);
 	stream.close_stream();
 	return 0;
 }
